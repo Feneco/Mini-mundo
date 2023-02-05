3f1ff51469f761bb94732627344dbe78f1afdce5
Wagner <wagnerweb2010@gmail.com>
2023-02-05 15:54:39 -0300

    Formatação

--- delete.sql
+++ delete.sql
@@ -1,14 +1,14 @@
-select tb_usuario.id_usuario as id_usuario_to_delete,
-       tb_dados.id_dados as id_dados_to_delete,
+select tb_usuario.id_usuario   as id_usuario_to_delete,
+       tb_dados.id_dados       as id_dados_to_delete,
        tb_endereco.id_endereco as id_endereco_to_delete,
        tb_telefone.id_telefone as id_telefone_to_delete,
-       tb_email.id_email as id_email_to_delete
+       tb_email.id_email       as id_email_to_delete
 from tb_usuario
-left join tb_dados     on tb_dados.id_dados = tb_usuario.tb_dados_id_dados
-inner join ta_moradia  on tm.tb_dados_id_dados = dados.id_dados
-left join tb_endereco  on tb_endereco.id_endereco = ta_moradia.tb_endereco_id_endereco
-left join tb_telefone  on tb_telefone.tb_dados_id_dados = dados.id_dados
-left join tb_email     on tb_email.tb_dados_id_dados = dados.id_dados
+left join  tb_dados     on tb_dados.id_dados             = tb_usuario.tb_dados_id_dados
+inner join ta_moradia   on tm.tb_dados_id_dados          = dados.id_dados
+left join  tb_endereco  on tb_endereco.id_endereco       = ta_moradia.tb_endereco_id_endereco
+left join  tb_telefone  on tb_telefone.tb_dados_id_dados = dados.id_dados
+left join  tb_email     on tb_email.tb_dados_id_dados    = dados.id_dados
 where tb_usuario.id_usuario = codigo_usuario;
 
 
--- views.sql
+++ views.sql
@@ -13,27 +13,27 @@ create view endereco_usuario as (
 		   e.estado, c.cidade, b.bairro, 
 		   endereco.cep, endereco.numero, endereco.complemento
 	from tb_dados dados
-    left join tb_usuario  u        on u.tb_dados_id_dados        = dados.id_dados
-	left join tb_telefone t        on dados.id_dados             = t.tb_dados_id_dados
+    left join  tb_usuario  u        on u.tb_dados_id_dados        = dados.id_dados
+	left join  tb_telefone t        on dados.id_dados             = t.tb_dados_id_dados
 	inner join ta_moradia  tm       on tm.tb_dados_id_dados       = dados.id_dados
-	left join tb_endereco endereco on tm.tb_endereco_id_endereco = endereco.id_endereco
-	left join tb_bairro   b        on b.id_bairro                = endereco.tb_bairro_id_bairro
-	left join tb_cidade   c        on c.id_cidade                = b.tb_cidade_id_cidade
-	left join tb_estado   e        on e.id_estado                = c.tb_estado_id_estado
+	left join  tb_endereco endereco on tm.tb_endereco_id_endereco = endereco.id_endereco
+	left join  tb_bairro   b        on b.id_bairro                = endereco.tb_bairro_id_bairro
+	left join  tb_cidade   c        on c.id_cidade                = b.tb_cidade_id_cidade
+	left join  tb_estado   e        on e.id_estado                = c.tb_estado_id_estado
 );
 
 create view usuarios_que_doaram as (
     select u.id_usuario, d.nome, i.id_item, i.item, i.descricao, td.qtd_saida as qtd
     from tb_usuario u
-    left join tb_dados d on d.id_dados = u.tb_dados_id_dados
-    inner join ta_doacao td on td.tb_usuario_id_usuario = u.id_usuario
-    left join tb_item i on td.tb_item_id_item = i.id_item
+    left join  tb_dados d    on d.id_dados               = u.tb_dados_id_dados
+    inner join ta_doacao td  on td.tb_usuario_id_usuario = u.id_usuario
+    left join  tb_item i     on td.tb_item_id_item       = i.id_item
 );
 
 create view usuarios_que_receberam as (
     select u.id_usuario, d.nome, i.id_item, i.item, i.descricao, tr.qtd_entrada as qtd
     from tb_usuario u
-    left join tb_dados d on d.id_dados = u.tb_dados_id_dados
-    inner join ta_recebe tr on tr.tb_usuario_id_usuario = u.id_usuario
-    left join tb_item i on tr.tb_item_id_item = i.id_item
+    left join  tb_dados d    on d.id_dados               = u.tb_dados_id_dados
+    inner join ta_recebe tr  on tr.tb_usuario_id_usuario = u.id_usuario
+    left join  tb_item i     on tr.tb_item_id_item       = i.id_item
 );
